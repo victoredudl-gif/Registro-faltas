d63e75b9c894b53ff8d369ae51ec8e94ba62779a
victoredudl-gif <victoredudl@gmail.com>
2025-10-22 02:11:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ try:
     st.success("✅ openpyxl está instalado correctamente.")
 except ImportError:
     st.error("❌ openpyxl NO está instalado en el entorno.")
+try:
+    import openpyxl
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
+    import openpyxl
 
 
 st.set_page_config(page_title="Registro de Faltas", layout="centered")
@@ -144,6 +149,7 @@ else:
     st.info("No hay coincidencias con la búsqueda.")
 
 #Reconstrucion forzada para instalar openpyxl
+
 
 
 
