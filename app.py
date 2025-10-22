e3e1a4f6e6575b8aa172facc059b0319aafab705
victoredudl-gif <victoredudl@gmail.com>
2025-10-22 02:14:38 -0400

    app.py

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ try:
     st.success("✅ openpyxl está instalado correctamente.")
 except ImportError:
     st.error("❌ openpyxl NO está instalado en el entorno.")
-try:
+ import subprocess
+    import sys
+
     import openpyxl
 except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
     import openpyxl
-
 
 st.set_page_config(page_title="Registro de Faltas", layout="centered")
 st.title("📋 Registro de Faltas Escolares")
@@ -149,6 +150,7 @@ else:
     st.info("No hay coincidencias con la búsqueda.")
 
 #Reconstrucion forzada para instalar openpyxl
+
 
 
 
